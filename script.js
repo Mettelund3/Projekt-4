cf59c11394cd68b3415fd5af76eb7a98a1ba229b
Maximilian90 <105641605+Maximilian90@users.noreply.github.com>
2022-10-09 16:22:19 +0200

    sletted ubrugbar kode Navbar

--- script.js
+++ script.js
@@ -1,34 +1,8 @@
 const   body = document.querySelector("body"),
         nav = document.querySelector("nav"),
-        modeToggle = document.querySelector(".dark-light"),
-        searchToggle = document.querySelector(".searchToggle"),
         sidebarOpen = document.querySelector(".sidebarOpen"),
         sidebarClose = document.querySelector(".sidebarClose");
-
-        let getMode = localStorage.getItem("mode");
-            if(getMode && getMode === "dark-mode")
-                body.classList.add("dark");
         
-
-// Js code to toggle dark and light mode
-        modeToggle.addEventListener("click" , () => {
-            modeToggle.classList.toggle("active");
-            body.classList.toggle("dark");
-
-
-//Js code to keep user selected mode even page refresh or file reopen
-            if(!body.classList.contains("dark")){
-                localStorage.setItem("mode" , "light-mode");
-            }else{
-                localStorage.setItem("mode" , "dark-mode");
-            }
-        });
-
-// Js code to toggle search box
-        searchToggle.addEventListener("click" , () => {
-            searchToggle.classList.toggle("active");
-        });
-
 
 // Js code to toggle sidebar
         sidebarOpen.addEventListener("click" , () =>{
@@ -42,7 +16,5 @@ const   body = document.querySelector("body"),
                 nav.classList.remove("active"); 
         });
 
-let searchable = ["Odense", "Nyborg", "Aarhus", "Fredercia"];
 
-const searchInput = document.getElementById('search');
 
